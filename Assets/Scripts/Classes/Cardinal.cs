dc256e724a33e9a422ab5e29b76c2d65a537de93
Ro4m3r <linus.beerman@gmail.com>
2018-11-07 14:53:01 +0100

    Refactored Cardinal Class

--- Assets/Scripts/Classes/Cardinal.cs
+++ Assets/Scripts/Classes/Cardinal.cs
@@ -2,38 +2,31 @@ using UnityEngine;
 
 public static class Cardinal
 {
-    public const int SOUTH = 0;
-    public const int SOUTHEAST = 1;
-    public const int EAST = 2;
-    public const int NORTHEAST = 3;
-    public const int NORTH = 4;
-    public const int NORTHWEST = 5;
-    public const int WEST = 6;
-    public const int SOUTHWEST = 7;
+    public enum Direction { South, SouthEast, East, NorthEast, North, NorthWest, West, SouthWest }
 
-    public static int calculateFacingPositionForSpeed(Vector2 speed)
+    public static Direction CalculateFacingPositionForSpeed(Vector2 speed)
     {
         if (speed.x == 0)
         {
-            if (speed.y < 0) return SOUTH;
-            if (speed.y > 0) return NORTH;
+            if (speed.y < 0) return Direction.South;
+            if (speed.y > 0) return Direction.North;
         }
         if (speed.y == 0)
         {
-            if (speed.x < 0) return WEST;
-            if (speed.x > 0) return EAST;
+            if (speed.x < 0) return Direction.West;
+            if (speed.x > 0) return Direction.East;
         }
         if (speed.y > 0)
         {
-            if (speed.x > 0) return NORTHEAST;
-            if (speed.x < 0) return NORTHWEST;
+            if (speed.x > 0) return Direction.NorthEast;
+            if (speed.x < 0) return Direction.NorthWest;
         }
         if (speed.y < 0)
         {
-            if (speed.x > 0) return SOUTHEAST;
-            if (speed.x < 0) return SOUTHWEST;
+            if (speed.x > 0) return Direction.SouthEast;
+            if (speed.x < 0) return Direction.SouthWest;
         }
 
-        return SOUTH;
+        return Direction.South;
     }
 }
--- Assets/Scripts/MainCharacter.cs
+++ Assets/Scripts/MainCharacter.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class MainCharacter : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    private int previousLookPosition;
-    private Animator animator;
-    public int lookPosition;
-    public float joystickDeadZone = 0.05f;
-    public float walkSpeed = 1.0f;
-    public float runSpeed = 2.0f;
 
+    [SerializeField]
+    private float _joystickDeadZone = 0.05f;
+    [SerializeField]
+    private float _walkSpeed = 1.0f;
+    [SerializeField]
+    private float _runSpeed = 2.0f;
+
+    private Cardinal.Direction _lookPosition;
+    private Rigidbody2D _rb;
+    private Animator _animator;
 
     void Start()
     {
-        this.rb = this.GetComponent<Rigidbody2D>();
-        this.animator = this.GetComponent<Animator>();
-        this.lookPosition = Cardinal.SOUTH;
-        this.previousLookPosition = Cardinal.SOUTH;
+        _rb = this.GetComponent<Rigidbody2D>();
+        _animator = this.GetComponent<Animator>();
+        _lookPosition = Cardinal.Direction.South;
     }
 
     void Update()
@@ -26,41 +28,45 @@ public class MainCharacter : MonoBehaviour
         // This implements Golden Sun's 8 cardinal point clamping even with analog joysticks
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        if (Mathf.Abs(x) > this.joystickDeadZone)
+        if (Mathf.Abs(x) > _joystickDeadZone)
         {
             x = x > 0 ? 1.0f : -1.0f;
         }
-        if (Mathf.Abs(y) > this.joystickDeadZone)
+        if (Mathf.Abs(y) > _joystickDeadZone)
         {
             y = y > 0 ? 1.0f : -1.0f;
         }
 
         // Make sure the magnitude of the speed vector never exceeds the walkSpeed.
         // This is done because of diagonal speeds being higher that straight ones.
-        Vector2 speed = Vector2.ClampMagnitude(new Vector2(x, y), this.walkSpeed);
+        Vector2 speed = Vector2.ClampMagnitude(new Vector2(x, y), this._walkSpeed);
 
         // Check if player is running
         bool run = Input.GetAxis("Fire1") > 0.0f;
         if (run)
         {
-            speed = speed * this.runSpeed;
+            speed = speed * this._runSpeed;
         }
 
         // Animation triggering
-        this.animator.SetFloat("speedMagnitude", speed.magnitude);
+        _animator.SetFloat("speedMagnitude", speed.magnitude);
         if (speed.magnitude > 0)
         {
-            this.previousLookPosition = this.lookPosition;
-            this.lookPosition = Cardinal.calculateFacingPositionForSpeed(speed);
-            if (this.previousLookPosition != this.lookPosition)
+            if (_lookPosition != Cardinal.CalculateFacingPositionForSpeed(speed))
             {
+                _lookPosition = Cardinal.CalculateFacingPositionForSpeed(speed);
                 // There was a cardinal point location. Change animation
-                this.animator.SetInteger("lookPosition", this.lookPosition);
-                this.animator.SetTrigger("changeCardinal");
+                this._animator.SetInteger("lookPosition", (int)_lookPosition);
+                this._animator.SetTrigger("changeCardinal");
             }
         }
 
         // Final player movement
-        this.rb.MovePosition(this.rb.position + speed * Time.deltaTime);
+        _rb.MovePosition(this._rb.position + speed * Time.deltaTime);
+    }
+
+    private void SetSpeed()
+    {
+
     }
 }
